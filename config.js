23234bd470f48ca49a9b6afd82272b26e35c6812
Van-Toan <87267049+Van-Toan@users.noreply.github.com>
2021-07-11 19:33:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'LU iu dấu à!',
+    introTitle: 'Bé uyên iu dấu à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
